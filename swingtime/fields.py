f00d0b44422bec7913ff1692090363120c8a9607
Aaron Johnson <aarontropy@gmail.com>
2013-09-18 08:09:15 -0400

    Added RRuleWeekdayListField field

--- swingtime/fields.py
+++ swingtime/fields.py
@@ -4,7 +4,7 @@ from dateutil import rrule
 # rrule.MO = 0, etc.
 weekdays = [rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR, rrule.SA, rrule.SU]
 
-class RRuleWeekdayField(models.Field):
+class RRuleWeekdayField(models.IntegerField):
 
 	description = "Model field to contain dateutil.rrule.weekday object"
 
@@ -16,3 +16,22 @@ class RRuleWeekdayField(models.Field):
 
 	def get_prep_value(self, value):
 		return value.weekday
+
+
+class RRuleWeekdayListField(models.CharField):
+	'''
+	Weekdays will be stored in a charfield as a comma-separated list of integers
+
+	'''
+
+	description = "A list of dateutil.rrule.weekday objects"
+
+	def to_python(self,value):
+		if isinstance(value, list):
+			return value
+
+		return [int(s) for s in value.split(',')]
+
+	def get_prep_value(self, value):
+		return ','.join([str(d) for d in value])
+
