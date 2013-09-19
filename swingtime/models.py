1762af6419707a2e034e0c68976c92719d90139e
Aaron Johnson <aarontropy@gmail.com>
2013-09-19 07:35:30 -0400

    Switched Events for Occurrences, renamed, dropped Occurrences and added generate_events

--- swingtime/models.py
+++ swingtime/models.py
@@ -3,6 +3,7 @@ from datetime import datetime, date, timedelta
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.forms.models import model_to_dict
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
@@ -50,8 +51,12 @@ class EventRule(models.Model):
     '''
     calendar = models.ForeignKey(Calendar, related_name="rules")
 
-    freq = models.PositiveIntegerField()
-    dtstart = models.DateTimeField()
+    title = models.CharField(_('title'), max_length=100)
+    start_time = models.DateTimeField(_('start time'))
+    end_time = models.DateTimeField(_('end time'), null=True)
+    all_day = models.BooleanField(_('all day'), default=False)
+
+    freq = models.PositiveIntegerField(default=RRule.WEEKLY)
     until = models.DateTimeField()
     count = models.PositiveIntegerField()
     interval = models.PositiveIntegerField()
@@ -68,6 +73,41 @@ class EventRule(models.Model):
     def __unicode__(self):
         return "Event rule for %s" % self.calendar.title
 
+    #---------------------------------------------------------------------------
+    def generate_events(self):
+        '''
+        Creates a set of events based on the options stored
+        Deletes previously-created child events and replaces with newly-created 
+        ones, event if the old events have been edited.
+
+        '''
+        rrule_params = model_to_dict(self, fields=['freq','until','count','interval','wkst','byweekday']) 
+
+        if not rrule_params['count'] and not rrule_params['until']:
+            rrule_params['count'] = 1
+        else:
+            if not self.all_day and self.end_time is not None:
+                delta = self.end_time - self.start_time
+            else:
+                delta = 0
+
+            evs = rrule.rrule(dtstart=self.start_time, **rrule_params)
+            if len(evs) > 0:
+                # Since we've successfully created a list of events from our
+                # rrule params, delete existing events linked to this rule.
+                # This removes all edits a user has made, so this is not
+                # ideal.
+                for evo in self.events:
+                    evo.delete()
+
+            for ev in evs:
+                self.events.create(
+                    start_time=ev, 
+                    end_time=ev + delta if self.end_time is not None else None,
+                    all_day=self.all_day,
+                    title=self.title,
+                    calendar=self.calendar,
+                )
 
 
 
@@ -116,11 +156,45 @@ class EventType(models.Model):
 
 #===============================================================================
 class Event(models.Model):
+    calendar = models.ForeignKey(Calendar, related_name=_('events'))
+    rule = models.ForeignKey(EventRule, related_name=_('events'))
+    event_type = models.ForeignKey(EventType, verbose_name=_('event type'))
+
+    title = models.CharField(_('title'), max_length=100)
+    notes = generic.GenericRelation(Note, verbose_name=_('notes'))
+    start_time = models.DateTimeField(_('start time'))
+    end_time = models.DateTimeField(_('end time'))
+    all_day = models.BooleanField(_('all day'))
+
+
+    #===========================================================================
+    class Meta:
+        verbose_name = _('event')
+        verbose_name_plural = _('events')
+        ordering = ('start_time', 'end_time', 'title')
+
+    #---------------------------------------------------------------------------
+    def __unicode__(self):
+        return u'%s: %s' % (self.title, self.start_time.isoformat())
+
+    #---------------------------------------------------------------------------
+    @models.permalink
+    def get_absolute_url(self):
+        return ('swingtime-event', [str(self.id)])
+
+    #---------------------------------------------------------------------------
+    def __cmp__(self, other):
+        return cmp(self.start_time, other.start_time)
+
+
+
+#===============================================================================
+class cEvent(models.Model):
     '''
     Container model for general metadata and associated ``Occurrence`` entries.
     '''
-    title = models.CharField(_('title'), max_length=32)
     description = models.CharField(_('description'), max_length=100)
+    title = models.CharField(_('title'), max_length=32)
     event_type = models.ForeignKey(EventType, verbose_name=_('event type'))
     notes = generic.GenericRelation(Note, verbose_name=_('notes'))
 
